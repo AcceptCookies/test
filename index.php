ff85a4ed72ae9f67fb79c2ed581159e9109072ee
AcceptCookies <ivanjesensky1987@gmail.com>
2018-09-25 09:47:54 +0200

    Prvy github commit Pridané echo cherry (cherry picked from commit cb7a020)

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 <?php echo 'testikzvzvzvzxv';
 echo 'bla alalalalalala'; ?>
 =======
-<?php echo 'testik'; ?>
+<?php echo 'testik';
+    echo 'cherry';
+    ?>
 >>>>>>> cb7a020... Prvy github commit
+
